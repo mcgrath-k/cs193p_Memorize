23c5a2d6ef897ed60b022c0b2ed930777e59ba13
Kyle McGrath <x11kjm@gmail.com>
2023-12-23 18:48:35 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kyle McGrath on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
